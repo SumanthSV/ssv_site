168bb0103237d96fcf3774ff70a8fe2bfb42124e
SumanthSV <158209234+SumanthSV@users.noreply.github.com>
2024-04-22 01:49:45 +0530

    Add files via upload

---
+++ src/components/Certificates.jsx
@@ -0,0 +1,73 @@
+import React,{ useEffect } from "react"
+import c from '../assets/certificate/C_nptel.jpg'
+import java from '../assets/certificate/java_hr.jpg'
+import ps from '../assets/certificate/problem_solving.jpg'
+import gen_ai from '../assets/certificate/gen_ai.jpg'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+
+
+const Certificates = () =>{
+    useEffect(()=>{
+        AOS.init({duration:1000})
+    })
+
+    const Cert=()=>[
+        {
+            id:1,
+            src:c,
+            title:'Intoduction to Programming in c',
+            issued:"https://nptel.ac.in/noc/E_Certificate/NPTEL23CS02S4440018203121906",
+        },
+        {
+            id:2,
+            src:java,
+            title:'Java',
+            issued:"https://www.hackerrank.com/certificates/543a87a08bfd?utm_medium=email&utm_source=mail_template_1393&utm_campaign=hrc_skills_certificate",
+        },
+        {
+            id:3,
+            src:ps,
+            title:'Problem Solving',
+            issued:"https://www.hackerrank.com/certificates/db10a75ce316",
+        },
+        {
+            id:4,
+            src:gen_ai,
+            title:'Generative AI',
+            issued:"https://www.cloudskillsboost.google/public_profiles/f7f0dca3-4443-4c98-8913-3790670848a3/badges/5780754",
+        }
+    ]
+
+    return (
+        <div name="certificates" className='h-auto max-h-screen-lg  bg-gradient-to-b from-black to-gray-800 w-full text-white'>
+      <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+        <div className='pb-8'>
+          <h2 className='text-4xl font-bold inline border-b-4 border-gray-400 '>Certificates</h2>
+          <p className='py-6'> I am certified in following programming languages  and technologies.</p>
+        </div>
+        <div   className='grid sm:grid-cols-2 md:grid-cols-2 mb-20 gap-16 px-12 sm:px-0 '>
+          {
+            Cert().map(({ id, src, title, issued}) => (
+
+              <div data-aos="fade-in" data-aos-duration="15000" key={id} className='shadow-md shadow-green-600 rounded-lg'>
+                <img src={src} alt="Loading" className='rounded-md duration-200 hover:scale-105' />
+                <p className='p-2 text-justify font-extralight md:text-center'>{title}</p>
+                <div className='flex items-center justify-center'>
+                  
+                  <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105  bg-gradient-to-l from-gray-700 to-blue-900 rounded-md'>
+                    <a target='_blank' href={issued}>View</a>
+                  </button>
+                </div>
+              </div>
+
+            ))
+          }
+        </div>
+      </div>
+    </div>
+    )
+
+}
+
+export default Certificates
